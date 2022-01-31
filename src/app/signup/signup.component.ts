21e8daccb45f19683067e70d6e1609e39c874041
anit <anitkumar@excellencetechnologies.in>
2022-01-31 16:32:55 +0530

    polling app

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -10,33 +10,48 @@ import { Router } from '@angular/router'
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-  signupForm= new FormGroup({
-    username: new FormControl('',[Validators.required]),
-    password: new FormControl('',[Validators.required]),
-    repassword: new FormControl('',[Validators.required]),
+  signupForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    repassword: new FormControl('', [Validators.required]),
     role: new FormControl('')
-  },{validators: PasswordValidator});
-  
- get username(){
-   return this.signupForm.get('username');
- }
- get password(){
-   return this.signupForm.get('password');
- }
- get repassword(){
-  return this.signupForm.get('repassword');
-}
+  }, { validators: PasswordValidator });
 
-  constructor(private signupService: SignupService,private _router: Router) { }
+  showme = false;
+  loginForm: any;
+  loginService: any;
 
-  ngOnInit(): void { 
+  get username() {
+    return this.signupForm.get('username');
   }
-  signup(){
-    this.signupService.signup(this.signupForm.value.username, this.signupForm.value.password,this.signupForm.value.role).subscribe((data: any)=>{
-      console.log(data,this.signupForm.value);
-      localStorage.setItem('token',data.token);
-      this._router.navigate(['../login'])
+  get password() {
+    return this.signupForm.get('password');
+  }
+  get repassword() {
+    return this.signupForm.get('repassword');
+  }
+
+  constructor(private signupService: SignupService, private _router: Router) { }
+
+  ngOnInit(): void {
+  }
+  signup() {
+    this.signupService.signup(this.signupForm.value.username, this.signupForm.value.password, this.signupForm.value.role).subscribe((data: any) => {
+      console.log(data, this.signupForm.value);
+      localStorage.setItem('token', data.token);
+
+      if (data.error) {
+        this.showme = true;
+      }
+
+     else {
+        const res = this.loginService.login(this.loginForm.value.username, this.loginForm.value.password);
+        if (data.token) {
+          this._router.navigate (['../dashboard']);
+        }
+      }
     }
+
     )
-}
+  }
 }
