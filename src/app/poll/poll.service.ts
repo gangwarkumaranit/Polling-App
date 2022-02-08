8f04ed1ed0d0cc3e38ef7be61b9b53b658ba567d
anit <anitkumar@excellencetechnologies.in>
2022-02-08 12:53:53 +0530

    polling app

--- src/app/poll/poll.service.ts
+++ src/app/poll/poll.service.ts
@@ -8,12 +8,12 @@ export class VoteService {
   constructor(private httpClient: HttpClient) { }
 
   public vote(_id: string, option_text: string) {
-    const access_token = localStorage.getItem('access_token')
+    const access_token = localStorage.getItem("access_token") ?? ""
     const httpOptions = {
         headers: new HttpHeaders({
           'access_token':  access_token
         })
       };
-      return this.httpClient.get(`https://secure-refuge-14993.herokuapp.com/do_vote?id=577212fdd1bba33c17b5b64e&option_text=nodejs _id=${_id}&option_text=${option_text}`, httpOptions);
+      return this.httpClient.get(`https://secure-refuge-14993.herokuapp.com/do_vote?id=${_id}&option_text=${option_text}`, httpOptions);
 }
 }
